c17f49cfe2560623578326a248b841404c19fcc0
StrugglingOwl <brucelou@live.com>
2017-05-23 12:26:42 +0800

    implement records#create

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,4 +6,17 @@ class RecordsController < ApplicationController
   def new
     @record = Record.new
   end
+
+  def create
+    @record = Record.new(record_params)
+    @record.save
+
+      redirect_to records_path
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:device_name, :description)
+  end
 end
