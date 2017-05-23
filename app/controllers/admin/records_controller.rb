d65185ebf3616e33d63f1f53447e37580e2a48c0
StrugglingOwl <brucelou@live.com>
2017-05-23 21:30:31 +0800

    implement admin CRUD

---
+++ app/controllers/admin/records_controller.rb
@@ -0,0 +1,55 @@
+class Admin::RecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+
+  def show
+    @record = Record.find(params[:id])
+  end
+
+  def index
+    @records = Record.all
+  end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      redirect_to admin_records_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      redirect_to admin_records_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+
+    @record.destroy
+
+    redirect_to admin_records_path
+  end
+
+
+  private
+
+  def record_params
+    params.require(:record).permit(:device_name, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :records
+  end
+
   resources :records
   root 'records#index'
 end
