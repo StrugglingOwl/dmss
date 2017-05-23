db774f7466922f85dc5da743763e8b7c990a81c1
StrugglingOwl <brucelou@live.com>
2017-05-23 12:17:43 +0800

    replace root with records#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :records
-  root 'welcome#index'
+  root 'records#index'
 end
