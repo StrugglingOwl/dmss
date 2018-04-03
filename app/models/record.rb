c6535e669940b020d748e5931f8721ec92215739
StrugglingOwl <brucelou@live.com>
2018-04-03 14:35:19 +0800

    limit picture extension:jpg/gif/png & size:5MB, add jQuery code to check picture size and show warning message

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,9 +7,16 @@ class Record < ApplicationRecord
   validates :maintenance, presence: true
   validates :result, presence: true
   validates :service_engineer, presence: true
-
+  scope :recent, -> { order("created_at DESC")}
+  
   # 告诉carrierwave把图像和模型关联起来
   mount_uploader :picture, PictureUploader
-
-  scope :recent, -> { order("created_at DESC")}
+  # 限制上传图片大小
+  validate :picture_size
+  # 验证上传的图片大小
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
