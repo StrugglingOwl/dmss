ea5fad0a4c8fbaaa89e04bfc3e590952b357d65e
StrugglingOwl <brucelou@live.com>
2018-06-06 02:21:21 +0800

    add post and hook record/user relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :record
-
-  validates :content, presence: true
-
-  scope :recent, -> { order("created_at DESC")}
-
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,6 @@
 class Record < ApplicationRecord
   belongs_to :user
+  has_many :posts
   validates :device_name, presence: true
   validates :case_number, presence: true
   validates :date, presence: true
@@ -8,7 +9,7 @@ class Record < ApplicationRecord
   validates :result, presence: true
   validates :service_engineer, presence: true
   scope :recent, -> { order("created_at DESC")}
-  
+
   # 告诉carrierwave把图像和模型关联起来
   mount_uploader :picture, PictureUploader
   # 限制上传图片大小
