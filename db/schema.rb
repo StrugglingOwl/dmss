c172d1c5918510f1deb39744b9899266e2230f96
StrugglingOwl <brucelou@live.com>
2018-06-28 21:05:29 +0800

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628120027) do
+ActiveRecord::Schema.define(version: 20180605171035) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20180628120027) do
     t.text     "remarks"
     t.string   "service_engineer"
     t.string   "picture"
-    t.string   "keyword"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,14 +57,6 @@ ActiveRecord::Schema.define(version: 20180628120027) do
     t.boolean  "is_admin",               default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "workflows", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "record_id"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
